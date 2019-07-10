2f6db788d2d851451ee88b68540b052f19fb9e75
Nathaniel Chien <nchien2@stanford.edu>
2019-07-10 13:14:32 -0700

    Changed method of updating colors to depend on hidden div, improve style

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ colors = {
 }
 
 print(xyDf)
+print(indices)
 
 app.layout = html.Div([
     html.H2(children = 'Insert Title Here', style={'text-align':'center'}),
@@ -46,23 +47,44 @@ app.layout = html.Div([
     html.Div(id='hidden-div', style={'display':'none'})
 ])
 
+# Callback for updating which points are selected
+# Saves state of selection in hidden div as string
+@app.callback(Output('hidden-div', 'children'),
+              [Input('main-scatter', 'clickData')],
+              [State('hidden-div', 'children')])
+def storeSelected(clickData, pastData):
+    activatedPoints = []
+    if pastData and pastData != ' ':
+        activatedPoints = list(map(int, pastData.split(' ')))
 
+    if clickData:
+        clickIndex = clickData['points'][0]['pointIndex']
+        if clickIndex in activatedPoints:
+            activatedPoints.remove(clickIndex)
+        else:
+            activatedPoints.append(clickIndex)
+
+    activatedString = ' '.join(str(i) for i in activatedPoints)
+    return (activatedString)
+
+
+# Callback for updating the main
 @app.callback(Output('main-scatter', 'figure'),
               [Input('color-options', 'value'),
-               Input('main-scatter', 'clickData')],
+               Input('hidden-div', 'children')],
               [State('main-scatter', 'figure')])
-def updateGraph(colorOption, clickData, figureData):
+def updateGraph(colorOption, selectedPoints, figureData):
+    #retrieves list of selectedpoints from hidden div
+    selectedList = []
+    if selectedPoints:
+        selectedList = list(map(int, selectedPoints.split(' ')))
+
+    #Change graph color scheme based on radio item selection
     if colorOption == 'gradient':
         clickmode = 'event+select'
-        if colorOption:
-            colorOption = figureData['data'][0]['marker']['color']
-        if clickData:
-            for point in clickData['points']:
-                index = point['pointIndex']
-                if(colorOption[index] == 'gray'):
-                    colorOption[index] = 'hsl({},60,70)'.format(colors[xyDf['Class'][index]])
-                else:
-                    colorOption[index] = 'gray'
+        colorOption = ['gray'] * xyDf.size
+        for index in selectedList:
+            colorOption[index] = 'hsl({},60,70)'.format(colors[xyDf['Class'][index]])
     else:
         colorOption = ['gray'] * xyDf.size
         clickmode = 'none'
@@ -93,25 +115,6 @@ def updateGraph(colorOption, clickData, figureData):
             clickmode=clickmode
         )
     }
-
-
-@app.callback(Output('hidden-div', 'children'),
-              [Input('main-scatter', 'clickData')],
-              [State('hidden-div', 'children')])
-def knnColoring(clickData, pastData):
-    activatedPoints = []
-    if pastData and pastData != ' ':
-        activatedPoints = list(map(int, pastData.split(' ')))
-
-    if clickData:
-        clickIndex = clickData['points'][0]['pointIndex']
-        if clickIndex in activatedPoints:
-            activatedPoints.remove(clickIndex)
-        else:
-            activatedPoints.append(clickIndex)
-
-    activatedString = ' '.join(str(i) for i in activatedPoints)
-    return (activatedString)
 
 
 if __name__ == '__main__':
